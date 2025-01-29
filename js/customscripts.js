ffde5f7981f6210cf4ec83b4061c6e719bb51972
Pius Willi <pius.willi@digitecgalaxus.ch>
2025-01-29 22:13:46 +0100

    add pagecontent from json

--- js/customscripts.js
+++ js/customscripts.js
@@ -1,17 +1,12 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
-    testTemplate = document.getElementById('test-template').innerHTML;
-    var compiledTestTemplate = Handlebars.compile(testTemplate);
-    var context = { title: "My New Post"};
-    var html = compiledTestTemplate(context);
-    document.getElementById('test-handlebarplaceholder').innerHTML = html;
+    var request = new XMLHttpRequest();
+    request.open("GET", "data/pagecontent.json", false);
+    request.send(null)
+    var pageData = JSON.parse(request.responseText);
+
+    navigationTemplate = document.getElementById('navigation-template').innerHTML;
+    var compiledNavigationTemplate = Handlebars.compile(navigationTemplate);
+    var navigationHtml = compiledNavigationTemplate(pageData);
+    document.getElementById('navbarResponsive').innerHTML = navigationHtml;
 });
