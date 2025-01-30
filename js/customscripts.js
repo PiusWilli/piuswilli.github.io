93a3432be0d1ff2cfb0bdb5646f4b16e7a72dd5c
Pius Willi <pius.willi@digitecgalaxus.ch>
2025-01-30 20:06:37 +0100

    make modal renderd dynamically

--- js/customscripts.js
+++ js/customscripts.js
@@ -8,16 +8,19 @@ window.addEventListener('DOMContentLoaded', event => {
     // load and add map 
     var mapRequest = new XMLHttpRequest();
     mapRequest.open("GET", "data/chilbi-map.svg", false);
-    mapRequest.send(null)
-    document.getElementById('map-placeholder').innerHTML = mapRequest.responseText
+    mapRequest.send(null);
+    document.getElementById('map-placeholder').innerHTML = mapRequest.responseText;
 
-    navigationTemplate = document.getElementById('clubs-template').innerHTML;
-    var compiledNavigationTemplate = Handlebars.compile(navigationTemplate);
-    var navigationHtml = compiledNavigationTemplate(pageData);
-    document.getElementById('clubs-placeholder').innerHTML = navigationHtml;
-
-    navigationTemplate = document.getElementById('navigation-template').innerHTML;
-    var compiledNavigationTemplate = Handlebars.compile(navigationTemplate);
-    var navigationHtml = compiledNavigationTemplate(pageData);
-    document.getElementById('navbarResponsive').innerHTML = navigationHtml;
+    renderTemplateToPlaceholder('clubs-template', 'clubs-placeholder', pageData);
+    renderTemplateToPlaceholder('navigation-template', 'navbarResponsive', pageData);
+    renderTemplateToPlaceholder('modal-template', 'modal-placeholder', pageData);
+    
 });
+
+var renderTemplateToPlaceholder = function(tempalteElementName, palaceHolderElementId, data)
+{
+    template = document.getElementById(tempalteElementName).innerHTML;
+    var compiledtemplate = Handlebars.compile(template);
+    var htmlCode = compiledtemplate(data);
+    document.getElementById(palaceHolderElementId).innerHTML = htmlCode;
+};
