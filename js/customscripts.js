efa920281533a9dcaac58b522902d37011ff4451
Pius Willi <pius.willi@digitecgalaxus.ch>
2025-02-07 18:09:51 +0100

    clean up javascript file

--- js/customscripts.js
+++ js/customscripts.js
@@ -1,12 +1,11 @@
-window.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener('DOMContentLoaded', () => {
     var request = new XMLHttpRequest();
     request.open("GET", "data/pagecontent.json", false);
     request.send(null)
     var pageData = JSON.parse(request.responseText);
 
     // load and add map 
-    d3.svg("/data/chilbi-map.svg").then(function(xml) {
+    d3.svg("/data/chilbi-map.svg").then((xml) => {
         d3.select("#map-container1").node().appendChild(xml.documentElement);
         prepareSvgZoom();
         initOpenModalOnClick(pageData.clubs);
@@ -20,66 +19,63 @@ window.addEventListener('DOMContentLoaded', event => {
     renderTemplateToPlaceholder('schedule-template', 'schedule-placeholder', pageData.schedule);
 });
 
-var renderTemplateToPlaceholder = function(tempalteElementName, palaceHolderElementId, data)
-{
+function renderTemplateToPlaceholder(tempalteElementName, palaceHolderElementId, data) {
     template = document.getElementById(tempalteElementName).innerHTML;
     var compiledtemplate = Handlebars.compile(template);
     var htmlCode = compiledtemplate(data);
     document.getElementById(palaceHolderElementId).innerHTML = htmlCode;
 };
 
-var prepareSvgZoom = function() {
-
+function prepareSvgZoom() {
     function handleZoom(e) {
         d3.select('#map-container1 svg #g1')
             .attr('transform', e.transform);
     }
-
     var svg = d3.select("#map-container1 svg");
     svg.call(d3.zoom().scaleExtent([0.8, 5]).on('zoom', handleZoom));
 };
 
-var resetSvgZoom = function(){
+function resetSvgZoom() {
     d3.select('#map-container1 svg #g1').attr('transform', null);
     var zoom = d3.zoom();
     var svg = d3.select('#map-container1 svg');
-    zoom.scaleTo(svg,1);
-    zoom.translateTo(svg, 400, 350); 
+    zoom.scaleTo(svg, 1);
+    zoom.translateTo(svg, 400, 350);
 }
 
-var setModalToggleAttributesOnElement = function(elementId, modalLink){
+function setModalToggleAttributesOnElement(elementId, clubid) {
     var pathElementClub1 = d3.select("#" + elementId);
     pathElementClub1.classed('club-link');
     pathElementClub1.attr('data-bs-toggle', 'modal');
-    pathElementClub1.attr('href', modalLink);
+    pathElementClub1.attr('href', '#clubModal' + clubid);
 };
 
-var initOpenModalOnClick = function(clubs){
-    if(clubs != null){
-        clubs.forEach(setMotalTogglesForClub);
+function initOpenModalOnClick(clubs) {
+    if (clubs != null) {
+        clubs.forEach(setModalToggleForClubs);
     }
 }
 
-function setMotalTogglesForClub(club){
-    if(club.svgPath != null){
-        club.svgPath.forEach((path) => setModalToggleAttributesOnElement(path, '#clubModal' + club.id));
+function setModalToggleForClubs(club) {
+    if (club.svgPath != null) {
+        club.svgPath.forEach((path) => setModalToggleAttributesOnElement(path, club.id));
     }
 }
 
-function blink(elementId){
+function blink(elementId) {
     var element = d3.select("#" + elementId);
-    var otherPins = d3.selectAll('.club-pin').filter(function() { return this.id !== ("#" + elementId); });
+    var otherPins = d3.selectAll('.club-pin').filter(function () { return this.id !== ("#" + elementId); });
     otherPins.transition().duration(900).style("opacity", "0");
     element.classed("spinner-grow", true);
-    setTimeout(function(){element.classed("spinner-grow", false);  otherPins.transition().duration(800).style("opacity", "100");},3000);
+    setTimeout(function () { element.classed("spinner-grow", false); otherPins.transition().duration(800).style("opacity", "100"); }, 3000);
 }
 
-function scrollTo(elementId){
+function scrollTo(elementId) {
     var element = document.getElementById(elementId);
     element.scrollIntoView();
 }
 
-function highlightSvgElement(elementId){
+function highlightSvgElement(elementId) {
     resetSvgZoom();
     scrollTo("map-placeholder");
     blink(elementId);
